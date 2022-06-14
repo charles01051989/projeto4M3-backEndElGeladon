f6cc75252f0faff9269cb0238a6c1e9138b84bd7
Charles Marques <charles_sjp@yahoo.com.br>
2022-06-13 23:34:39 -0300

    ok

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 
-const port = process.env.PORT || 3001;
+const port = 3001;
 const app = express();
 
 const cors = require('cors');
@@ -10,19 +10,19 @@ const routes = require('./src/routes/paleta.route');
 
 const connectToDataBase = require('./src/database/database');
 
-connectToDataBase();
+connectToDataBase()
 
 app.use(express.json());
 
-app.use((req, res, next) => {
+// app.use((req, res, next) => {
   
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-  app.use(cors());
-  next();
-});
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+//   app.use(cors());
+//   next();
+// });
 
-// app.use(cors());
+app.use(cors());
 app.use('/paletas', routes);
 
 app.listen(port, () => {
--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectToDataBase = () => {
-  mongoose.connect(process.env.DATABASE_URI, {
+  mongoose.connect('mongodb://localhost:27017/paletas-db', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }).then(() => console.log("MongoDb Connected!")).catch((error) => console.log(`Connection error with MongoDb, error: ${error}`))
